a6d301fd5390ee25d89f9d3ff647530864887f38
snake <bpchaoxin@163.com>
2011-11-30 10:20:57 +0800

    Initial Commit

---
+++ Control Fun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Control Fun
+//
+//  Created by snake on 11-11-30.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Control Fun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Control Fun
+//
+//  Created by snake on 11-11-30.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
