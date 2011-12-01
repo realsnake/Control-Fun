1843f925ad0ab76377dfaeec8e30371687527d9e
snake <bpchaoxin@163.com>
2011-12-01 20:21:34 +0800

    slider switch alert

--- Control Fun/ViewController.h
+++ Control Fun/ViewController.h
@@ -8,6 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIActionSheetDelegate>
+{
+    UITextField *nameField;
+    UITextField *numberField;
+    UILabel     *sliderLabel;
+    UISwitch    *leftSwitch;
+    UISwitch    *rightSwitch;
+    UIButton    *doSomethingButton;
+}
+
+@property (retain, nonatomic) IBOutlet UITextField *nameField;
+@property (retain, nonatomic) IBOutlet UITextField *numberField;
+@property (retain, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (retain, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (retain, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (retain, nonatomic) IBOutlet UIButton *doSomethingButton;
+
+-(IBAction)textFieldDoneEditing:(id)sender;
+-(IBAction)backgroundTap:(id)sender;
+-(IBAction)sliderChanged:(id)sender;
+-(IBAction)switchChanged:(id)sender;
+-(IBAction)taggleControls:(id)sender;
+-(IBAction)buttonPressed:(id)sender;
 
 @end
--- Control Fun/ViewController.m
+++ Control Fun/ViewController.m
@@ -7,8 +7,96 @@
 //
 
 #import "ViewController.h"
+#define kSwitchesSegmentIndex 0
 
 @implementation ViewController
+@synthesize nameField;
+@synthesize numberField;
+@synthesize sliderLabel;
+@synthesize leftSwitch;
+@synthesize rightSwitch;
+@synthesize doSomethingButton;
+
+
+-(IBAction)taggleControls:(id)sender
+{
+    if ([sender selectedSegmentIndex] == kSwitchesSegmentIndex) 
+    {
+        leftSwitch.hidden = NO;
+        rightSwitch.hidden = NO;
+        doSomethingButton.hidden = YES;
+    }
+    else
+    {
+        leftSwitch.hidden = YES;
+        rightSwitch.hidden = YES;
+        doSomethingButton.hidden = NO;
+    }
+}
+
+-(IBAction)switchChanged:(id)sender
+{
+    UISwitch *whichSwitch = (UISwitch *)sender;
+    BOOL setting = whichSwitch.isOn;
+    [leftSwitch setOn:setting animated:YES];
+    [rightSwitch setOn:setting animated:YES];
+}
+
+//actionSheet let user decide whether to proceed alert
+-(IBAction)buttonPressed:(id)sender
+{
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+                                  initWithTitle:@"Are you sure?"
+                                  delegate:self
+                                  cancelButtonTitle:@"No Way!"
+                                  destructiveButtonTitle:@"Yes, I'm sure!"
+                                  otherButtonTitles: nil];
+    [actionSheet showInView:self.view];
+    [actionSheet release];
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex != [actionSheet cancelButtonIndex]) {
+        NSString *msg = nil;
+        if (nameField.text.length > 0) {
+            msg = [[NSString alloc] initWithFormat:@"You can breathe easy, %@, (^ @ ^) everything gose well!", nameField.text];
+        }
+        else
+        {
+            msg = @"You can breathe easy, everything went OK!";
+        }
+    UIAlertView *alert = [[UIAlertView alloc]
+                         initWithTitle:@"Something was done."                         
+                         message:msg 
+                         delegate:self
+                         cancelButtonTitle:@"Phew!"
+                         otherButtonTitles:nil];
+    [alert show];
+    [alert release];
+    [msg release];
+    }
+}
+
+-(IBAction)sliderChanged:(id)sender
+{
+    UISlider *slider = (UISlider *)sender;
+    int progressAsInt = (int)(slider.value + 0.5);
+    NSString *newText = [[NSString alloc] initWithFormat:@"%d", progressAsInt];
+    sliderLabel.text = newText;
+    [newText release];
+    
+}
+-(IBAction)textFieldDoneEditing:(id)sender
+{
+    [sender resignFirstResponder];
+}
+
+-(IBAction)backgroundTap:(id)sender
+{
+    [nameField resignFirstResponder];
+    [numberField resignFirstResponder];
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,10 +110,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImage *buttonImageNormal = [UIImage imageNamed:@"whiteButton.png"];
+    UIImage *stretchableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [doSomethingButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    UIImage *buttonImagePressed = [UIImage imageNamed:@"blueButton.png"];
+    UIImage *stretchableButtonImagePressed = [buttonImagePressed stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [doSomethingButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
 }
 
 - (void)viewDidUnload
 {
+    [self setNameField:nil];
+    [self setNumberField:nil];
+    [self setSliderLabel:nil];
+    [self setLeftSwitch:nil];
+    [self setRightSwitch:nil];
+    [self setDoSomethingButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -61,4 +161,13 @@
     }
 }
 
+- (void)dealloc {
+    [nameField release];
+    [numberField release];
+    [sliderLabel release];
+    [leftSwitch release];
+    [rightSwitch release];
+    [doSomethingButton release];
+    [super dealloc];
+}
 @end
